03d8c95ed4bd9c6b6e714f09667001e5dc07ed6c
pgits <petergits@gmail.com>
2019-11-22 14:18:32 -0600

    Create Package.swift
    tag is 7.9.1

---
+++ Package.swift
@@ -0,0 +1,17 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "mparticle-apple-integration-adobe",
+    platforms: [.iOS(.v9)],
+    // platforms: [.iOS("9.0"), .macOS("10.10"), tvOS("9.0"), .watchOS("2.0")],
+    products: [
+        .library(name: "mparticle-apple-integration-adobe", targets: ["mparticle-apple-integration-adobe"])
+    ],
+    targets: [
+        .target(
+            name: "mparticle-apple-integration-adobe",
+            path: "mParticle-Adobe-Media"
+        )
+    ]
+)
